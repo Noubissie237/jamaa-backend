76724cb632f1373cb7d2e5a8344e2187dbec5a0c
ornelnana4 <ornelnana4@gmail.com>
2025-05-16 16:02:56 +0100

    Avancement leger

--- service-account/src/main/java/com/jamaa/service_account/resolver/AccountMutationResolver.java
+++ service-account/src/main/java/com/jamaa/service_account/resolver/AccountMutationResolver.java
@@ -1,7 +1,7 @@
 package com.jamaa.service_account.resolver;
 
 import java.io.IOException;
-
+import java.math.BigDecimal;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
@@ -20,5 +20,9 @@ public class AccountMutationResolver {
     @MutationMapping
     public Account createAccount(@Argument Long userId) throws IOException {
         return accountService.createAccount(userId);
+    }
+    @MutationMapping
+    public Account incrementBalance(@Argument BigDecimal amount ,@Argument Long accountId) throws IOException{
+        return accountService.incrementBalance(accountId,amount);
     }
 }
